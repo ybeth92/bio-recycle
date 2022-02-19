a7f2847b447abdafaa69bd4cb34cd9c64951c4a7
Ybeth Chavez <ybth.92.15@gmail.com>
2022-02-18 21:09:48 -0500

    add: list cities

--- src/migration/1645061903287-initialState.ts
+++ src/migration/1645061903287-initialState.ts
@@ -1,12 +1,16 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class initialState1644790393137 implements MigrationInterface {
-    name = 'initialState1644790393137'
+export class initialState1645061903287 implements MigrationInterface {
+    name = 'initialState1645061903287'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE \`services\` (\`id\` int NOT NULL AUTO_INCREMENT, \`business\` varchar(150) NOT NULL, \`name\` varchar(150) NOT NULL, \`price\` decimal(10,2) NOT NULL DEFAULT '0.00', PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+        await queryRunner.query(`CREATE TABLE \`exchanges\` (\`id\` int NOT NULL AUTO_INCREMENT, \`createdAt\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`discount\` decimal(10,2) NOT NULL DEFAULT '0.00', \`totalAmount\` decimal(10,2) NOT NULL DEFAULT '0.00', \`code\` varchar(20) NOT NULL, \`user_id\` int NULL, \`service_id\` int NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`roles\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(40) NOT NULL, UNIQUE INDEX \`IDX_648e3f5447f725579d7d4ffdfb\` (\`name\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`users\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(50) NOT NULL, \`lastName\` varchar(40) NOT NULL, \`mothersLastName\` varchar(40) NOT NULL, \`dni\` varchar(8) NOT NULL, \`email\` varchar(100) NOT NULL, \`password\` varchar(128) NOT NULL, \`phone\` varchar(9) NOT NULL, \`createdAt\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` timestamp(6) NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`isActive\` tinyint NOT NULL DEFAULT 1, \`adress\` varchar(250) NOT NULL, \`point\` int NOT NULL, \`city_id\` int NULL, \`role_id\` int NULL, UNIQUE INDEX \`IDX_5fe9cfa518b76c96518a206b35\` (\`dni\`), UNIQUE INDEX \`IDX_97672ac88f789774dd47f7c8be\` (\`email\`), UNIQUE INDEX \`IDX_a000cca60bcf04454e72769949\` (\`phone\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`cities\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(120) NOT NULL, UNIQUE INDEX \`IDX_a0ae8d83b7d32359578c486e7f\` (\`name\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+        await queryRunner.query(`ALTER TABLE \`exchanges\` ADD CONSTRAINT \`FK_e9163a94e04700841ca4067a209\` FOREIGN KEY (\`user_id\`) REFERENCES \`users\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE \`exchanges\` ADD CONSTRAINT \`FK_7e410f04477d948c16c61c519d6\` FOREIGN KEY (\`service_id\`) REFERENCES \`services\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE \`users\` ADD CONSTRAINT \`FK_03934bca2709003c5f08fd436d2\` FOREIGN KEY (\`city_id\`) REFERENCES \`cities\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE \`users\` ADD CONSTRAINT \`FK_a2cecd1a3531c0b041e29ba46e1\` FOREIGN KEY (\`role_id\`) REFERENCES \`roles\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
@@ -14,6 +18,8 @@ export class initialState1644790393137 implements MigrationInterface {
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE \`users\` DROP FOREIGN KEY \`FK_a2cecd1a3531c0b041e29ba46e1\``);
         await queryRunner.query(`ALTER TABLE \`users\` DROP FOREIGN KEY \`FK_03934bca2709003c5f08fd436d2\``);
+        await queryRunner.query(`ALTER TABLE \`exchanges\` DROP FOREIGN KEY \`FK_7e410f04477d948c16c61c519d6\``);
+        await queryRunner.query(`ALTER TABLE \`exchanges\` DROP FOREIGN KEY \`FK_e9163a94e04700841ca4067a209\``);
         await queryRunner.query(`DROP INDEX \`IDX_a0ae8d83b7d32359578c486e7f\` ON \`cities\``);
         await queryRunner.query(`DROP TABLE \`cities\``);
         await queryRunner.query(`DROP INDEX \`IDX_a000cca60bcf04454e72769949\` ON \`users\``);
@@ -22,6 +28,8 @@ export class initialState1644790393137 implements MigrationInterface {
         await queryRunner.query(`DROP TABLE \`users\``);
         await queryRunner.query(`DROP INDEX \`IDX_648e3f5447f725579d7d4ffdfb\` ON \`roles\``);
         await queryRunner.query(`DROP TABLE \`roles\``);
+        await queryRunner.query(`DROP TABLE \`exchanges\``);
+        await queryRunner.query(`DROP TABLE \`services\``);
     }
 
 }
